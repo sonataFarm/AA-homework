a72619c14c21f0bf42b558a672ffae1f6cc3bb74
Nate Festinger <nate.festinger@gmail.com>
2017-08-14 20:53:21 -0700

    Add W3D5 HW

---
+++ W3D5/meta_corgis.rb
@@ -0,0 +1,140 @@
+class SnackBox
+  SNACK_BOX_DATA = {
+    1 => {
+      "bone" => {
+        "info" => "Phoenician rawhide",
+        "tastiness" => 20
+      },
+      "kibble" => {
+        "info" => "Delicately braised hamhocks",
+        "tastiness" => 33
+      },
+      "treat" => {
+        "info" => "Chewy dental sticks",
+        "tastiness" => 40
+      }
+    },
+    2 => {
+      "bone" => {
+        "info" => "An old dirty bone",
+        "tastiness" => 2
+      },
+      "kibble" => {
+        "info" => "Kale clusters",
+        "tastiness" => 1
+      },
+      "treat" => {
+        "info" => "Bacon",
+        "tastiness" => 80
+      }
+    },
+    3 => {
+      "bone" => {
+        "info" => "A steak bone",
+        "tastiness" => 64
+      },
+      "kibble" => {
+        "info" => "Sweet Potato nibbles",
+        "tastiness" => 45
+      },
+      "treat" => {
+        "info" => "Chicken bits",
+        "tastiness" => 75
+      }
+    }
+  }
+  def initialize(data = SNACK_BOX_DATA)
+    @data = data
+  end
+
+  def get_bone_info(box_id)
+    @data[box_id]["bone"]["info"]
+  end
+
+  def get_bone_tastiness(box_id)
+    @data[box_id]["bone"]["tastiness"]
+  end
+
+  def get_kibble_info(box_id)
+    @data[box_id]["kibble"]["info"]
+  end
+
+  def get_kibble_tastiness(box_id)
+    @data[box_id]["kibble"]["tastiness"]
+  end
+
+  def get_treat_info(box_id)
+    @data[box_id]["treat"]["info"]
+  end
+
+  def get_treat_tastiness(box_id)
+    @data[box_id]["treat"]["tastiness"]
+  end
+end
+
+class CorgiSnacks
+
+  def initialize(snack_box, box_id)
+    @snack_box = snack_box
+    @box_id = box_id
+  end
+
+  def bone
+    info = @snack_box.get_bone_info(@box_id)
+    tastiness = @snack_box.get_bone_tastiness(@box_id)
+    result = "Bone: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+
+  def kibble
+    info = @snack_box.get_kibble_info(@box_id)
+    tastiness = @snack_box.get_kibble_tastiness(@box_id)
+    result = "Kibble: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+
+  def treat
+    info = @snack_box.get_treat_info(@box_id)
+    tastiness = @snack_box.get_treat_tastiness(@box_id)
+    result = "Treat: #{info}: #{tastiness} "
+    tastiness > 30 ? "* #{result}" : result
+  end
+end
+
+class MetaCorgiSnacks
+  def initialize(snack_box, box_id)
+    @snack_box = snack_box
+    @box_id = box_id
+  end
+
+  def self.define_snack(*types)
+    types.each do |type|
+      define_method(type) do
+        info = @snack_box.send("get_#{type}_info", @box_id)
+        tastiness = @snack_box.send("get_#{type}_tastiness", @box_id)
+        result = "#{type.upcase}: #{info}: #{tastiness} "
+        tastiness > 30 ? "* #{result}" : result
+      end
+    end
+  end
+
+  define_snack :bone, :kibble, :treat
+
+  # def method_missing(method_name, *args)
+  #   component = method_name.to_s
+  #
+  #   taste_method_name = "get_#{method_name}_tastiness".to_sym
+  #   tastiness = @snack_box.send(taste_method_name, @box_id)
+  #
+  #   info_method_name = "get_#{method_name}_info".to_sym
+  #   info = @snack_box.send(info_method_name, @box_id)
+  #
+  #   result = "#{component.capitalize}: #{info}: #{tastiness} "
+  #   tastiness > 30 ? "* #{result}" : result
+  # end
+
+
+  def self.define_snack(name)
+    # Your code goes here...
+  end
+end
