e4ae79dd2dfddab2f7eac385232dfa6b244ed56d
Nate Festinger <nate.festinger@gmail.com>
2017-09-01 12:39:37 -0700

    Add W5D5 HW

---
+++ W5D5/hw1.js
@@ -0,0 +1,20 @@
+window.setTimeout(() => alert('HAMMERTIME'), 5000);
+
+function hammerTime(time) {
+  window.setTimeout(() => alert(`${time} is hammertime!`), time);
+}
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+reader.question('Would you like some tea?\n', (teaResponse) => {
+  console.log(teaResponse);
+  reader.question('Would you like biscuits?\n', (biscuitResponse) => {
+    console.log(`So you ${teaResponse} want tea and you ${biscuitResponse} want biscuits.`);
+    reader.close();
+  });
+});
